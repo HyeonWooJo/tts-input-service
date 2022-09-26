d6b1515679b745f4fd4cd1423df4c418f22ab367
HyeonWooJo <chaduri7913@gmail.com>
2022-09-26 17:15:53 +0900

    Feat: 어드민 추가

--- backend/apis/admin.py
+++ backend/apis/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(User)
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['id','project_title','user','created_at','updated_at']
+
+
+@admin.register(Audio)
+class AudioAdmin(admin.ModelAdmin):
+    list_display = ['id','project','identifier','speed','audio_file']
+
+
+@admin.register(Text)
+class TextAdmin(admin.ModelAdmin):
+    list_display = ['id','idx', 'audio','text']
